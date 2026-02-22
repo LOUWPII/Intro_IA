160b8fd14f7749c94eb797abccf9cf2689dd60fe
LOUWPII <148557091+LOUWPII@users.noreply.github.com>
2026-02-21 23:31:04 -0500

    Add files via upload
    se añadió:
    sangre pura, muggle y mestizo. Además estuve investigando y en prolog hay un operador "!" que permite decirle al compilador que se comprometa con el camino "actual" del arbol de recursión. Es decir que no hace mas backtracking. Cambié la regla de sangre pura para que siguiera la filosofia del nivel de sangre en las sagas de Harry P y fuera más estricta. Ahora, para que alguien sea puro, prolog revisa que el padre sea puro y que la madre también sea pura. El ! al final de esa regla asegura que si ya se confirmamos que alguien es puro, se deje de investigar el árbol de esa persona para el tema de la eficiencia.
    
    El ! sirvió para poder hacer las reglas de muggle y mestizo porqe se pusieron en orden las reglas. Es decir, primero se pregunta si es sangre pura, si no es sangre pura entonces se verifica si es muggle, y si no es muggle ni sangre pura pero tiene al menos un ancestro puro, entonces es mestizo, y ahí se solucioma el asunto con Harry, ya que el papá James es puro pero la mamá muggle.

---
+++ Harry_Potter_genealogico.pl
@@ -0,0 +1,315 @@
+% ==========================================
+% RELACIONES PATERNAS (es_padre)
+% ==========================================
+
+% Hijos de Phineas Nigellus Black
+es_padre(phineas_nigellus_black, sirius_black_sr).
+es_padre(phineas_nigellus_black, phineas_black).
+es_padre(phineas_nigellus_black, belvina_black).
+es_padre(phineas_nigellus_black, arcturus_black).
+es_padre(phineas_nigellus_black, cygnus_black_sr).
+
+% Hijos de Cygnus Black Sr.
+es_padre(cygnus_black_sr, dorea_black).
+es_padre(cygnus_black_sr, marius_black).
+es_padre(cygnus_black_sr, cassiopeia_black).
+es_padre(cygnus_black_sr, pollux_black).
+
+% Hijos de Sirius Black Sr.
+es_padre(sirius_black_sr, arcturus_black_ii).
+es_padre(sirius_black_sr, lycoris_black).
+es_padre(sirius_black_sr, regulus_black_sr).
+
+% Hijos de Arcturus Black
+es_padre(arcturus_black, cedrella_black).
+es_padre(arcturus_black, charis_black).
+es_padre(arcturus_black, callidora_black).
+
+% Hijos de Pollux Black
+es_padre(pollux_black, alphard_black).
+es_padre(pollux_black, walburga_black).
+es_padre(pollux_black, cygnus_black).
+
+% Hijos de Cygnus Black
+es_padre(cygnus_black, bellatrix_lestrange).
+es_padre(cygnus_black, andromeda_tonks).
+es_padre(cygnus_black, narcisa_malfoy).
+
+% Hijos de Arcturus Black II
+es_padre(arcturus_black_ii, orion_black).
+es_padre(arcturus_black_ii, lucretia_black).
+
+% Hijos de Orion Black
+es_padre(orion_black, sirius_black).
+es_padre(orion_black, regulus_black).
+
+% Hijos de Voldemort
+es_padre(tom_marvolo_riddle, delphini_riddle). % con Bellatrix
+
+% Hijos de Lucius Malfoy
+es_padre(lucius_malfoy, draco_malfoy).
+
+% Hijos de Remus Lupin
+es_padre(remus_lupin, ted_lupin).
+
+% Hijos de Ted Tonks
+es_padre(ted_tonks, nymphadora_tonks).
+
+% Hijos de Septimus Weasley
+es_padre(septimus_weasley, arthur_weasley).
+
+% Hijos de Arthur Weasley
+es_padre(arthur_weasley, fred_weasley).
+es_padre(arthur_weasley, charles_weasley).
+es_padre(arthur_weasley, percy_weasley).
+es_padre(arthur_weasley, william_weasley).
+es_padre(arthur_weasley, ronald_weasley).
+es_padre(arthur_weasley, ginny_weasley).
+
+% Hijos de William Weasley
+es_padre(william_weasley, victoire_weasley).
+es_padre(william_weasley, dominique_weasley).
+es_padre(william_weasley, louis_weasley).
+
+% Hijos de Ronald Weasley
+es_padre(ronald_weasley, rose_granger_weasley).
+es_padre(ronald_weasley, hugo_granger_weasley).
+
+% Hijos de Fleamont Potter
+es_padre(fleamont_potter, james_potter).
+
+% Hijos de James Potter
+es_padre(james_potter, harry_potter).
+
+% Hijos de Harry Potter
+es_padre(harry_potter, james_sirius_potter).
+es_padre(harry_potter, albus_severus_potter).
+es_padre(harry_potter, lily_luna_potter).
+
+% Hijos de Evan Dursley
+es_padre(evan_dursley, dudley_dursley).
+
+
+% ==========================================
+% RELACIONES MATERNAS (es_madre)
+% ==========================================
+
+% Hijos de Ursula Flint
+es_madre(ursula_flint, sirius_black_sr).
+es_madre(ursula_flint, phineas_black).
+es_madre(ursula_flint, belvina_black).
+es_madre(ursula_flint, arcturus_black).
+es_madre(ursula_flint, cygnus_black_sr).
+
+% Hijos de Violetta Bulstrode
+es_madre(violetta_bulstrode, dorea_black).
+es_madre(violetta_bulstrode, marius_black).
+es_madre(violetta_bulstrode, cassiopeia_black).
+es_madre(violetta_bulstrode, pollux_black).
+
+% Hijos de Hester Gamp
+es_madre(hester_gamp, arcturus_black_ii).
+es_madre(hester_gamp, lycoris_black).
+es_madre(hester_gamp, regulus_black_sr).
+
+% Hijos de Lysandra Yaxley
+es_madre(lysandra_yaxley, cedrella_black).
+es_madre(lysandra_yaxley, charis_black).
+es_madre(lysandra_yaxley, callidora_black).
+
+% Hijos de Irma Crabbe
+es_madre(irma_crabbe, alphard_black).
+es_madre(irma_crabbe, walburga_black).
+es_madre(irma_crabbe, cygnus_black).
+
+% Hijos de Druella Rosier
+es_madre(druella_rosier, bellatrix_lestrange).
+es_madre(druella_rosier, andromeda_tonks).
+es_madre(druella_rosier, narcisa_malfoy).
+
+% Hijos de Melania_macmillan
+es_madre(melania_macmillan, orion_black).
+es_madre(melania_macmillan, lucretia_black).
+
+% Hijos de Walburga Black
+es_madre(walburga_black, sirius_black).
+es_madre(walburga_black, regulus_black).
+
+% Hijos de Bellatrix Black
+es_madre(bellatrix_lestrange, delphini_riddle).
+
+% Hijos de Narcisa Black
+es_madre(narcisa_malfoy, draco_malfoy).
+
+% Hijos de Andromeda Black
+es_madre(andromeda_tonks, nymphadora_tonks).
+
+% Hijos de Nymphadora Tonks
+es_madre(nymphadora_tonks, ted_lupin).
+
+% Hijos de Cedrella Black
+es_madre(cedrella_black, arthur_weasley).
+
+% Hijos de Molly Weasley
+es_madre(molly_weasley, fred_weasley).
+es_madre(molly_weasley, charles_weasley).
+es_madre(molly_weasley, percy_weasley).
+es_madre(molly_weasley, william_weasley).
+es_madre(molly_weasley, ronald_weasley).
+es_madre(molly_weasley, ginny_weasley).
+
+% Hijos de Fleur Delacour
+es_madre(fleur_delacour, victoire_weasley).
+es_madre(fleur_delacour, dominique_weasley).
+es_madre(fleur_delacour, louis_weasley).
+
+% Hijos de Hermione Granger
+es_madre(hermione_granger, rose_granger_weasley).
+es_madre(hermione_granger, hugo_granger_weasley).
+
+% Hijos de Euphemia Potter
+es_madre(euphemia_potter, james_potter).
+
+% Hijos de Lily Potter
+es_madre(lily_potter, harry_potter).
+
+% Hijos de Ginny Weasley
+es_madre(ginny_weasley, james_sirius_potter).
+es_madre(ginny_weasley, albus_severus_potter).
+es_madre(ginny_weasley, lily_luna_potter).
+
+% Hijos de Petunia Dursley
+es_madre(petunia_dursley, dudley_dursley).
+
+
+% ==========================================
+% PAREJAS
+% ==========================================
+es_pareja(phineas_nigellus_black, ursula_flint).
+es_pareja(cygnus_black_sr, violetta_bulstrode).
+es_pareja(sirius_black_sr, hester_gamp).
+es_pareja(arcturus_black, lysandra_yaxley).
+es_pareja(pollux_black, irma_crabbe).
+es_pareja(cygnus_black, druella_rosier).
+es_pareja(arcturus_black_ii, melania_macmillan).
+es_pareja(orion_black, walburga_black).
+es_pareja(rodolphus_lestrange, bellatrix_lestrange).
+es_pareja(lucius_malfoy, narcisa_malfoy).
+es_pareja(ted_tonks, andromeda_tonks).
+es_pareja(remus_lupin, nymphadora_tonks).
+es_pareja(septimus_weasley, cedrella_black).
+es_pareja(ignatius_prewett, lucretia_black).
+es_pareja(arthur_weasley, molly_weasley).
+es_pareja(william_weasley, fleur_delacour).
+es_pareja(ronald_weasley, hermione_granger).
+es_pareja(harry_potter, ginny_weasley).
+es_pareja(fleamont_potter, euphemia_potter).
+es_pareja(james_potter, lily_potter).
+es_pareja(evan_dursley, petunia_dursley).
+es_pareja(tom_marvolo_riddle, bellatrix_lestrange).
+
+% Pureza de Sangre
+sangre_pura(phineas_nigellus_black).
+sangre_pura(ursula_flint).
+sangre_pura(rodolphus_lestrange).
+sangre_pura(druella_rosier).
+sangre_pura(lucius_malfoy).
+sangre_pura(irma_crabbe).
+sangre_pura(violetta_bulstrode).
+sangre_pura(hester_gamp).
+sangre_pura(melania_macmillan).
+sangre_pura(ignatius_prewett).
+sangre_pura(lysandra_yaxley).
+sangre_pura(septimus_weasley).
+sangre_pura(molly_weasley).
+sangre_pura(euphemia_potter).
+sangre_pura(fleamont_potter).
+familia_de_muggles(lily_potter).
+familia_de_muggles(petunia_dursley).
+familia_de_muggles(evan_dursley).
+familia_de_muggles(fleur_delacour).
+familia_de_muggles(ted_tonks).
+familia_de_muggles(tom_marvolo_riddle).
+familia_de_muggles(hermione_granger).
+
+% Familia Directa
+son_pareja(B,A):- es_pareja(A,B);es_pareja(B,A).
+es_hijo(B,A):- es_padre(A,B);es_madre(A,B).
+progenitor(B,A):- es_padre(B,A);es_madre(B,A).
+% Se creó la regla de es_padre y es_madre para la pureza y se verifique por ambos padres
+padre(P, H) :- es_padre(P, H).
+madre(M, H) :- es_madre(M, H).
+es_hermano(lily_potter, petunia_dursley).
+es_hermano(petunia_dursley,lily_potter).
+es_hermano(B,C):- progenitor(A,B), progenitor(A,C), B\= C.
+es_tio(C,A):- es_hermano(B,C), progenitor(B,A).
+es_sobrino(A,C):- es_tio(C,A).
+es_primo(A,B):- es_tio(C,A), progenitor(C,B).
+es_abuelo(A,C) :- progenitor(A,B), progenitor(B,C).
+es_bisabuelo(A,D) :- progenitor(A,B), progenitor(B,C), progenitor(C,D).
+es_tatarabuelo(A,E) :- progenitor(A,B), progenitor(B,C), progenitor(C,D), progenitor(D,E).
+es_trastatarabuelo(A,F) :- progenitor(A,B), progenitor(B,C), progenitor(C,D), progenitor(D,E), progenitor(E,F).
+es_ancestro(A, B) :- progenitor(A, B).
+es_ancestro(A, B) :- progenitor(A, C), es_ancestro(C, B).
+es_descendiente(A, B) :- es_hijo(A, B).
+es_descendiente(A, B) :- es_hijo(A, C), es_descendiente(C, B).
+
+es_sobrino_nieto(A,B):- es_tio(B,C), progenitor(C,A).
+es_sobrino_bisnieto(A,B):- es_tio(B,C), es_abuelo(C,A).
+es_sobrino_segundo(A,B):- es_primo(B,C), progenitor(C,A).
+es_sobrino_nieto_segundo(A,B):-es_primo(B,C), es_abuelo(C,A).
+es_tio_abuelo(A,B):- es_hermano(A,C), es_abuelo(C,B).
+es_tio_segundo(A,B):- progenitor(C,A), es_tio_abuelo(C,B).
+es_primo_segundo(A,B):- es_abuelo(C,A), es_tio_abuelo(C,B).
+es_tio_bisabuelo(A,B):- es_hermano(A,C), es_bisabuelo(C,B).
+es_tio_abuelo_segundo(A,B):- progenitor(C,A), es_tio_bisabuelo(C,B).
+es_tio_tercero(A,B):- es_abuelo(C,A), es_tio_bisabuelo(C,B).
+es_primo_tercero(A,B):- es_bisabuelo(C,A), es_tio_bisabuelo(C,B).
+es_tio_tatarabuelo(A,B):- es_hermano(A,C), es_tatarabuelo(C,B).
+es_tio_bisabuelo_segundo(A,B):- progenitor(C,A), es_tio_tatarabuelo(C,B).
+es_tio_abuelo_tercero(A,B):- es_abuelo(C,A), es_tio_tatarabuelo(C,B).
+es_tio_cuarto(A,B):- es_bisabuelo(C,A), es_tio_tatarabuelo(C,B).
+es_primo_cuarto(A,B):- es_tatarabuelo(C,A), es_tio_tatarabuelo(C,B).
+
+es_pariente(A,B) :- es_ancestro(C,A), es_ancestro(C,B).
+
+%Familia Politica
+es_cunado(A,B) :- (son_pareja(A,C),es_hermano(C,B));(es_hermano(A,C),son_pareja(C,B)).
+es_suegro(A,B) :- progenitor(A,C),son_pareja(C,B).
+
+/*
+es_sangre_pura(A) :- sangre_pura(A),\+ familia_de_muggles(A).
+es_sangre_pura(A) :- \+ familia_de_muggles(A), (progenitor(B,A), es_sangre_pura(B)).
+*/
+
+%---- ESTA SERÍA LA REGLA RECURSIVA -----
+%El caso base es si ya es un hecho qu A es de sangre pura.
+%y se usa el ! para que prolog no explore más opciones
+es_sangre_pura(A) :- 
+    sangre_pura(A), 
+    !. 
+% Segun la filosofia de Harry P, una persona es pura si sus padres son puros, y si esta persona no es de familia muggle 
+es_sangre_pura(A) :-
+    \+ familia_de_muggles(A),
+    padre(P, A), es_sangre_pura(P),
+    madre(M, A), es_sangre_pura(M),
+    !. % se usa el operador de corte para que prolog pare si se comprobaron ambos lados
+
+%--- ESTAS SERÍAN LAS REGLAS PARA CONSUKTAR EN LAS QUERIES
+% Caso es puro:
+
+nivel_sangre(A, pura) :- 
+    es_sangre_pura(A), 
+    !.
+
+% Caso muggle:
+nivel_sangre(A, muggle) :- 
+    familia_de_muggles(A), 
+    !.
+
+% Caso mestizo
+nivel_sangre(A, mestiza) :- 
+    % Tiene al menos un ancestro magico
+    (es_ancestro(Anc, A), sangre_pura(Anc)), 
+    % Y no es ni muggle ni puramente mago
+    !.
